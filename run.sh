e368c953afcd03e2247986d682ca4ddbcd4c2ac4
markice25 <markice@live.com>
2020-10-19 10:58:20 -0400

    update

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
  
 #!/bin/bash
 
-python3 ./src/data_process.py -i ./input/censustract-00-10.csv -o ./output/report.csv -c -1
+python3.7 ./src/data_process.py -i ./input/censustract-00-10.csv -o ./output/report.csv -c -1
